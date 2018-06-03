e8a12b19791b31f1d3fb2cf60a6a2aafba04a41f
Anindya <anindya.themaster@gmail.com>
2018-06-04 01:09:04 +0530

    1. redux, router installation 2. Container setup 3. Dashboard Container setup

---
+++ src/containers/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Header from '../../components/Header/header';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app" className="app">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
